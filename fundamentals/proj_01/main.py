b2c9ea474e13ecd1047d5cfad21b10b5a66d627a
patrickmwila <patrickmwila.org@gmail.com>
2021-12-04 16:28:55 +0200

    initial commit

---
+++ fundamentals/proj_01/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Author     : Patrick Mwila
+    Instructor : Dr. Angela Yu
+    Description: This program implements a simple band name generator.
+"""
+
+# Display message and prompt for user input
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city your grew up in?\n")
+
+pet = input("What's the name of your pet?\n")
+
+# display the results
+print("Your band name could be " + city + " " + pet)
+
